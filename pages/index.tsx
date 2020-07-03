cd14b8195a0f4a010997458f9a0245f184495f8b
zlatan <zlatan.pham@gmail.com>
2020-07-03 17:31:27 +0700

    fix: purgecss work incorrectly

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,37 +1,24 @@
 import React from 'react';
-import styled from 'styled-components';
-import tw from 'twin.macro';
-import Button from 'components/Button';
-import useSWR from 'swr';
 import Link from 'next/link';
-import APIs from 'libs/apis';
-
-const Title = styled.h1`
-  ${tw`bg-black text-white px-2 py-3 mt-2`}
-  font-size: 50px;
-`;
+import repos from 'constants/repo';
 
 export default () => {
-  const { data } = useSWR<string[]>(APIs.Repository.getURL, () =>
-    APIs.Repository.get(),
-  );
   return (
     <div>
-      <Title className="underline opacity-50">My page</Title>
-      <Button>Hello world</Button>
-
-      <h1>Trending Projects</h1>
-      <div>
-        {data
-          ? data.map((project) => (
-              <p key={project}>
-                <Link href="/[user]/[repo]" as={`/${project}`}>
-                  <a>{project}</a>
-                </Link>
-              </p>
-            ))
-          : 'loading...'}
-      </div>
+      <h1 className="text-4xl font-bold mb-8">Welcome</h1>
+      <p className="text-lg mb-8">
+        <a>Next Boilerplate</a> provides the basics to get a fast web app with
+        NextJS. Features:
+      </p>
+      <ul className="list-disc list-inside">
+        {repos.map((project) => (
+          <li key={project}>
+            <Link href="/[user]/[repo]" as={`/${project}`}>
+              <a>{project}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
--- postcss.config.js
+++ postcss.config.js
@@ -3,9 +3,6 @@ module.exports = {
     require('postcss-import'),
     require('tailwindcss'),
     require('postcss-preset-env'),
-    require('autoprefixer'),
-    require('postcss-clean')({
-      level: 2, // Merge duplicated declarations
-    }),
+    require('postcss-clean'),
   ],
 };
