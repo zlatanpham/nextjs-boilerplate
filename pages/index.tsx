d6b118c76c76f0d5b3e066b7934b0023fe7850ef
Thanh Pham <zlatan.pham@gmail.com>
2020-08-15 20:03:55 +0700

    feat: integrate eslint (#9)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import Link from 'next/link';
 import repos from 'constants/repo';
 
@@ -7,13 +6,13 @@ export default () => {
     <div>
       <h1 className="text-4xl font-bold mb-8">Welcome</h1>
       <p className="text-lg mb-8">
-        <a>Next Boilerplate</a> provides the basics to get a fast web app with
+        <b>Next Boilerplate</b> provides the basics to get a fast web app with
         NextJS. Features:
       </p>
       <ul className="list-disc list-inside">
         {repos.map((project) => (
           <li key={project}>
-            <Link href="/[user]/[repo]" as={`/${project}`}>
+            <Link as={`/${project}`} href="/[user]/[repo]">
               <a>{project}</a>
             </Link>
           </li>
