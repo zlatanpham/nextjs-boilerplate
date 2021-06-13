648500aa8203e5e876c18cc92c5d81a15aed347b
Thanh Pham <zlatan.pham@gmail.com>
2021-06-13 22:19:58 +0700

    chore: remove redundant path alias in next.config

--- next.config.js
+++ next.config.js
@@ -1,21 +1,6 @@
-const path = require('path');
 const withCSS = require('@zeit/next-css');
 require('dotenv').config();
 
 module.exports = withCSS({
-  webpack(config, options) {
-    [
-      'components',
-      'pages',
-      'utils',
-      'types',
-      'styles',
-      'utils',
-      'libs',
-    ].forEach(dir => {
-      config.resolve.alias[dir] = path.join(__dirname, dir);
-    });
-    return config;
-  },
   env: {},
 });
